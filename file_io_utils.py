77862441ca3cbcd557f71630408eb538fc75907e
Matas Remeika <matasr10@gmail.com>
2025-02-14 06:22:29 +0000

    Fixed quote bug

--- file_io_utils.py
+++ file_io_utils.py
@@ -9,7 +9,7 @@ def convert_list_to_txt(path, lines_list):
     with open(path, "w") as f:
         for line in lines_list:
             # .replace("\n", "\\n") preserves one entry – one line structure 
-            f.write(f"{line.replace("\n", "\\n")}\n") 
+            f.write(f'{line.replace("\n", "\\n")}\n') 
 
 # preds = extract_predictions_from_json("evaluations/abstracts_university_medium_max20_2024-12-12_21-37-41.106794")[:10]
 # convert_list_to_txt("dummy_test_pred.txt", preds)
