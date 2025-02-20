e442c664daab062bcd5758f309838a742b58b5df
Matas Remeika <matasr10@gmail.com>
2025-02-20 15:34:56 +0000

    Added missing parameter file

---
+++ parameters.py
@@ -0,0 +1,43 @@
+openai_api_key = ""
+openai_model = "gpt-4o-mini"
+# vllm_model = "meta-llama/Llama-3.1-8B-Instruct"
+# vllm_model = "hugging-quants/Meta-Llama-3.1-8B-Instruct-AWQ-INT4"
+vllm_model = "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4"
+
+algorithm_parameters = {
+    "DC": "University student",
+    "ILT": "Medium"
+}
+
+system_prompt = """
+You are a reader assistant. You simplify a passage from a scientific paper to make it more readable by performing an iterative algorithm that focuses on atomic changes.
+
+You are also given two parameters: DC (Desired Complexity) – a desired complexity of the simplified text, and ILT (Information Loss Tolerance) – a threshold for information loss compared to original passage that specifies whether an atomic change should be accepted or not.
+
+The algorithm is as follows:
+1. Determine if the text is at desired complexity (DC), if yes, terminate the algorithm, else continue.
+2. Identify a section of the text whose complexity is above DC.
+3. Propose a simplification of the identified section.
+4. Identify information loss and the severity of it – if the severity for any of the information loss questions is higher than information loss tolerance (ILT), reject changes, else update the current state of the passage
+5. Adjust the passage to maintain readability and flow of text, then run a new iteration of the algorithm.
+
+When the algorithm terminates, you print the final simplified passage
+
+When responding to prompts, only respond to the given question and do not proceed to the next step
+"""
+
+non_iterative_system_prompt = """
+You are a reader assistant. You simplify a passage from a scientific paper to make it more readable.
+
+You are also given two parameters: DC (Desired Complexity) – a desired complexity of the simplified text, and ILT (Information Loss Tolerance) – a threshold for information loss compared to original passage.
+
+Only print the simplified passage
+"""
+
+aiir_mistral_system_prompt = """
+You are a skilled editor, known for your ability to simplify complex text while preserving it. You explain the technical terms, defining what they are (e.g., terms like Blockchain, Cryptojacking, all abbreviations), without removing sentences or summarizing them.
+"""
+
+aiir_llama_run_1_system_prompt = """
+Simplify this text for English speaking science students in college. Maximize the use of simple words and short sentences, but include keywords from the original text. Optimize the output ROUGE, SARI, and BLEU scores
+"""
