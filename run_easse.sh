63b97e046ab534bc380cf340b90ab57395bf2ad7
Matas Remeika <matasr10@gmail.com>
2025-02-21 01:36:13 +0000

    Updated bash script to read recent files

---
+++ run_easse.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PREDICTION_FILE_NAME="type=sentence_dc=university_student_ilt=university_student_i=20_timestamp=2025-02-14_08-13-22.551088"
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <evaluate/report> <number of files to read>"
+    exit 1
+fi
+
+# Assign arguments to variables
+COMMAND="$1"
+FILE_COUNT="$2"
+
+# Validate that the second argument is an integer
+if ! [[ ${FILE_COUNT} =~ ^[0-9]+$ ]]; then
+    echo "Error: The second parameter must be an integer."
+    exit 1
+fi
+
+# Get the list of n most recent file paths in the directory
+FILES=$(ls "./predictions" -t | head -n ${FILE_COUNT})
+
+# Execute the command on each file path
+for FILE in $FILES; do
+    echo "FILE: ${FILE}"
+    # easse ${COMMAND} -q -t custom --refs_sents_paths "dataset/simpletext_lines/sentence_train_references_50.txt" --orig_sents_path "dataset/simpletext_lines/sentence_train_sources_50.txt" --sys_sents_path ${FILE}
+done
+
+
+# easse evaluate -q -t custom --refs_sents_paths "dataset/simpletext_lines/sentence_train_references_50.txt" --orig_sents_path "dataset/simpletext_lines/sentence_train_sources_50.txt" --sys_sents_path "predictions/${PREDICTION_FILE_NAME}"
--- run_evaluation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PREDICTION_FILE_NAME="type=sentence_dc=university_student_ilt=university_student_i=20_timestamp=2025-02-14_08-13-22.551088"
-
-easse evaluate -q -t custom --refs_sents_paths "dataset/simpletext_lines/sentence_train_references_50.txt" --orig_sents_path "dataset/simpletext_lines/sentence_train_sources_50.txt" --sys_sents_path "predictions/${PREDICTION_FILE_NAME}"
