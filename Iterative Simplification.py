0e9b20f75d946d79d798b26c27466f623ebc8381
Vejas Matas Remeika <matasr10@gmail.com>
2025-04-03 10:45:45 +0100

    Added unaware_iterative algorithm (limited context to only current iteration)

--- Iterative Simplification.py
+++ Iterative Simplification.py
@@ -37,6 +37,34 @@ def simplify_passage_iteratively(chat_bot, algorithm_parameters, max_iter=20):
         
         chat_bot.add_iteration_results()
 
+def simplify_passage_iteratively_unaware(chat_bot, algorithm_parameters, max_iter=20):
+    for _ in range(max_iter):
+        # chat_bot.send_prompt("Identify which parts of the passage are the most complex, then the complexity level of the passage. Limit your answer to a maximum of 5 sentences")
+        # chat_bot.send_prompt(f'Is determined complexity higher than DC ({algorithm_parameters["DC"]})?  Answer "Yes" or "No"')
+        
+        # # Add prompting to convert DC to DC (FKGL), alternatively, add it to algorithm_parameters
+        chat_bot.send_limited_context_prompt("Identify which parts of the passage are the most complex, then the complexity level of the passage. Limit your answer to a maximum of 5 sentences", 3)
+        chat_bot.send_limited_context_prompt(f'FKGL score of the latest passage version is {chat_bot.get_latest_fkgl()}. Is determined complexity higher than DC ({algorithm_parameters["DC"]})?  Answer "Yes" or "No"', 5)
+        if "NO" in chat_bot.get_last_response().upper()[-5:]:
+            break
+        chat_bot.send_limited_context_prompt(f'Identify a single complicated section of the passage. Remember to respect the ILT ({algorithm_parameters["ILT"]}) contraint. Only provide the identified section', 7)
+        chat_bot.send_limited_context_prompt("Simplify this section. Only provide the proposed simplification", 9)
+        chat_bot.send_limited_context_prompt("Reincorporate the simplified section into the passage", 11)
+        chat_bot.send_limited_context_prompt("Identify information loss and its severity in the updated passage compared to the original. Comparison must be between the originally provided (the very first) passage and the current simplified version. Limit your answer to a maximum of 5 sentences", 13)
+        chat_bot.send_limited_context_prompt(f'What is the highest severity level identified in your last answer? Is it higher than ILT ({algorithm_parameters["ILT"]})? Provide the highest severity level, followed by an answer to the ILT question as "Yes" or "No"', 15)
+        if "NO" in chat_bot.get_last_response().upper()[-5:]:
+            chat_bot.send_limited_context_prompt("Accept the proposed simplification. Print the updated version of the passage, do not print anything else", 17)
+            # chat_bot.send_limited_context_prompt("If needed, adjust the passage to maintain readabily and flow of text", 19)
+        else:
+            chat_bot.send_limited_context_prompt("Revert the last proposed change. In further iterations you may still attempt to simplify this section in other ways. Print the reverted passage, do not print anything else", 17)
+            
+
+        # Check if the model ran out of tokens, if so, ignore this iteration and terminate
+        if chat_bot.get_last_response() == "":
+            return
+        
+        chat_bot.add_iteration_results()
+
 # def simplify_passage_iteratively_condensed(chat_bot, system_prompt, algorithm_parameters, passage, max_iter=20):
 def simplify_passage_iteratively_condensed(chat_bot, algorithm_parameters, max_iter=20):
     for _ in range(max_iter):
@@ -307,11 +335,13 @@ passages_to_simplify = 5
 passage_type_to_simplify = "sentence"
 
 # simplify_passages("iterative", simplify_passage_iteratively, parameters.system_prompt, parameters.algorithm_parameters, passage_type_to_simplify, 20, passages_to_simplify)
-simplify_passages("condensed_iterative", simplify_passage_iteratively_condensed, parameters.system_prompt, parameters.algorithm_parameters, passage_type_to_simplify, 20, passages_to_simplify)
+simplify_passages("unaware_iterative", simplify_passage_iteratively_unaware, parameters.system_prompt, parameters.algorithm_parameters, passage_type_to_simplify, 20, passages_to_simplify)
+# simplify_passages("condensed_iterative", simplify_passage_iteratively_condensed, parameters.system_prompt, parameters.algorithm_parameters, passage_type_to_simplify, 20, passages_to_simplify)
 # simplify_passages("non_iterative", simplify_passage_non_iteratively, parameters.non_iterative_system_prompt, parameters.algorithm_parameters, passage_type_to_simplify, 0, passages_to_simplify)
 
 for passage_type_to_simplify in ["sentence", "abstract"]:
     # simplify_passages("iterative", simplify_passage_iteratively, parameters.system_prompt, parameters.algorithm_parameters, passage_type_to_simplify, 20, passages_to_simplify)
+    # simplify_passages("unaware_iterative", simplify_passage_iteratively_unaware, parameters.system_prompt, parameters.algorithm_parameters, passage_type_to_simplify, 20, passages_to_simplify)
     # simplify_passages("condensed_iterative", simplify_passage_iteratively_condensed, parameters.system_prompt, parameters.algorithm_parameters, passage_type_to_simplify, 20, passages_to_simplify)
     # simplify_passages("non_iterative", simplify_passage_non_iteratively, parameters.non_iterative_system_prompt, parameters.algorithm_parameters, passage_type_to_simplify, 0, passages_to_simplify)
     pass
