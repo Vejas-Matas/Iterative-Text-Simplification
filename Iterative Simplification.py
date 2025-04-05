26750665ec8b41d23cd3b32d0d67e300e1d93f6e
Vejas Matas Remeika <matasr10@gmail.com>
2025-04-05 15:54:35 +0100

    Added more explicit references to original and simplified passages in the prompts

--- Iterative Simplification.py
+++ Iterative Simplification.py
@@ -44,23 +44,23 @@ def simplify_passage_iteratively_unaware(chat_bot, algorithm_parameters, max_ite
         
         # # Add prompting to convert DC to DC (FKGL), alternatively, add it to algorithm_parameters
         latest_passage_version = chat_bot.get_iteration_results()[-1]["prediction"]
-        chat_bot.add_system_prompt("The lastest vetsion of the passage is provided in the following prompt")
+        chat_bot.add_system_prompt("The latest version of the simplified passage is provided in the following prompt")
         chat_bot.add_system_prompt(latest_passage_version)
-        chat_bot.send_limited_context_prompt("Identify which parts of the passage are the most complex, then the complexity level of the passage. Limit your answer to a maximum of 5 sentences", 2)
+        chat_bot.send_limited_context_prompt("Identify which parts of the simplified passage are the most complex, then the complexity level of the passage. Limit your answer to a maximum of 5 sentences", 2)
         # chat_bot.send_limited_context_prompt(f'FKGL score of the latest passage version is {chat_bot.get_latest_fkgl()}. Is determined complexity higher than DC ({algorithm_parameters["DC"]})?  Answer "Yes" or "No"', 4)
         chat_bot.send_limited_context_prompt(f'Is determined complexity higher than DC ({algorithm_parameters["DC"]})?  Answer "Yes" or "No"', 4)
         if "NO" in chat_bot.get_last_response().upper()[-5:]:
             break
-        chat_bot.send_limited_context_prompt(f'Identify a single complicated section of the passage. Remember to respect the ILT ({algorithm_parameters["ILT"]}) contraint. Only provide the identified section', 6)
+        chat_bot.send_limited_context_prompt(f'Identify a single complicated section of the simplified passage. Remember to respect the ILT ({algorithm_parameters["ILT"]}) contraint. Only provide the identified section', 6)
         chat_bot.send_limited_context_prompt("Simplify this section. Only provide the proposed simplification", 8)
-        chat_bot.send_limited_context_prompt("Reincorporate the simplified section into the passage", 10)
-        chat_bot.send_limited_context_prompt("Identify information loss and its severity in the updated passage compared to the original. Comparison must be between the originally provided (the very first) passage and the current simplified version. Limit your answer to a maximum of 5 sentences", 12)
+        chat_bot.send_limited_context_prompt("Reincorporate the simplified section into the simplified passage", 10)
+        chat_bot.send_limited_context_prompt("Identify information loss and its severity in the updated simplified passage compared to the original passage. Comparison must be between the originally provided (the very first) passage and the current simplified version with the proposed change. Limit your answer to a maximum of 5 sentences", 12)
         chat_bot.send_limited_context_prompt(f'What is the highest severity level identified in your last answer? Is it higher than ILT ({algorithm_parameters["ILT"]})? Provide the highest severity level, followed by an answer to the ILT question as "Yes" or "No"', 14)
         if "NO" in chat_bot.get_last_response().upper()[-5:]:
-            chat_bot.send_limited_context_prompt("Accept the proposed simplification. Print the updated version of the passage, do not print anything else", 16)
+            chat_bot.send_limited_context_prompt("Accept the proposed simplification. Print the updated version of the simplified passage, do not print anything else", 16)
             # chat_bot.send_limited_context_prompt("If needed, adjust the passage to maintain readabily and flow of text", 18)
         else:
-            chat_bot.send_limited_context_prompt("Revert the last proposed change. In further iterations you may still attempt to simplify this section in other ways. Print the reverted passage, do not print anything else", 16)
+            chat_bot.send_limited_context_prompt("Revert the last proposed change. Print the simplified passage with the proposed change reverted, do not print anything else", 16)
             
 
         # Check if the model ran out of tokens, if so, ignore this iteration and terminate
@@ -72,16 +72,16 @@ def simplify_passage_iteratively_unaware(chat_bot, algorithm_parameters, max_ite
 # def simplify_passage_iteratively_condensed(chat_bot, system_prompt, algorithm_parameters, passage, max_iter=20):
 def simplify_passage_iteratively_condensed(chat_bot, algorithm_parameters, max_iter=20):
     for _ in range(max_iter):
-        chat_bot.send_limited_context_prompt(f'FKGL score of the latest passage version is {chat_bot.get_latest_fkgl()}. Identify which parts of the passage are the most complex. Based on this information, identify the complexity of the passage. Is determined complexity higher than DC ({algorithm_parameters["DC"]})? Answer "Yes" or "No"', 3)
+        chat_bot.send_limited_context_prompt(f'FKGL score of the latest simplified passage version is {chat_bot.get_latest_fkgl()}. Identify which parts of the simplified passage are the most complex. Based on this information, identify the complexity of the simplified passage. Is determined complexity higher than DC ({algorithm_parameters["DC"]})? Answer "Yes" or "No"', 3)
         if "NO" in chat_bot.get_last_response().upper()[-5:]:
             break
-        chat_bot.send_limited_context_prompt(f'Identify a single complicated section of the passage. Simplify this section, and remember to respect the ILT ({algorithm_parameters["ILT"]}) contraint. Reincorporate the simplified section into the passage. Only provide the reincorporated version', 5)
-        chat_bot.send_limited_context_prompt(f'Identify information loss and its severity in the updated passage compared to the original. Comparison must be between the originally provided (the very first) passage and the current simplified version. Limit your answer to a maximum of 5 sentences. What is the highest severity level identified in your last answer? Is it higher than ILT ({algorithm_parameters["ILT"]})? Provide the highest severity level, followed by an answer to the ILT question as "Yes" or "No"', 7)
+        chat_bot.send_limited_context_prompt(f'Identify a single complicated section of the simplified passage. Simplify this section, and remember to respect the ILT ({algorithm_parameters["ILT"]}) contraint. Reincorporate the simplified section into the simplified passage. Only provide the reincorporated version', 5)
+        chat_bot.send_limited_context_prompt(f'Identify information loss and its severity in the updated simplified passage compared to the original passage. Comparison must be between the originally provided (the very first) passage and the current simplified version with the proposed change. Limit your answer to a maximum of 5 sentences. What is the highest severity level identified in your last answer? Is it higher than ILT ({algorithm_parameters["ILT"]})? Provide the highest severity level, followed by an answer to the ILT question as "Yes" or "No"', 7)
         if "NO" in chat_bot.get_last_response().upper()[-5:]:
-            chat_bot.send_limited_context_prompt("Accept the proposed simplification. Print the updated version of the passage, do not print anything else", 9)
+            chat_bot.send_limited_context_prompt("Accept the proposed simplification. Print the updated version of the simplified passage, do not print anything else", 9)
         #     chat_bot.send_limited_context_prompt("If needed, adjust the passage to maintain readabily and flow of text")
         else:
-            chat_bot.send_limited_context_prompt("Revert the last proposed change. In further iterations you may still attempt to simplify this section in other ways. Print the reverted passage, do not print anything else", 9)
+            chat_bot.send_limited_context_prompt("Revert the last proposed change. Print the simplified passage with the proposed change reverted, do not print anything else", 9)
 
         # Check if the model ran out of tokens, if so, ignore this iteration and terminate
         if chat_bot.get_last_response() == "":
@@ -121,7 +121,7 @@ def simplify_passages(algorithm_name, algorithm_fn, system_prompt, algorithm_par
 
         if algorithm_parameters is not None and algorithm_parameters != {}:
             algorithm_string = "\n".join(f"{parameter}: {value}" for parameter, value in algorithm_parameters.items())
-            chat_bot.add_system_prompt(f"Algorithm parameters are as follows.\n{algorithm_string}\n\nThe passage is provided in the following message")
+            chat_bot.add_system_prompt(f"Algorithm parameters are as follows.\n{algorithm_string}\n\nThe original passage is provided in the following message")
         else:
             chat_bot.add_system_prompt(f"The passage is provided in the following message")
 
@@ -145,7 +145,7 @@ def simplify_passages(algorithm_name, algorithm_fn, system_prompt, algorithm_par
         results.append(iteration_results)
 
         ### Save / display results
-        # chat_bot.print_chat()
+        chat_bot.print_chat()
         chat_bot.save_chat()
         # chat_bot.print_token_usage_log()
 
