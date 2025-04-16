91fcac39324a6de10d321b3c14c343034fedec6d
MatasRR <matasr10@gmail.com>
2025-04-16 18:24:42 +0000

    Upload results

--- easse_report_generator.sh
+++ easse_report_generator.sh
@@ -12,7 +12,7 @@ do
     do
         if [[ -e "$FILE" ]]; then
             echo "$FILE"
-            # easse report -p "graphs/reports/${FILE}.html" -t custom --refs_sents_paths "dataset/simpletext_lines/${PASSAGE_TYPE}_${DATA_TYPE}_references.txt" --orig_sents_path "dataset/simpletext_lines/${PASSAGE_TYPE}_${DATA_TYPE}_sources.txt" --sys_sents_path "./predictions/${FILE}"
+            easse report -p "${FILE}.html" -t custom --refs_sents_paths "dataset/simpletext_lines/${PASSAGE_TYPE}_${DATA_TYPE}_references.txt" --orig_sents_path "dataset/simpletext_lines/${PASSAGE_TYPE}_${DATA_TYPE}_sources.txt" --sys_sents_path "${FILE}"
         fi
     done
 done
